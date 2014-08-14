2efd581c095094ce757085ced410ac07dce48161
admin <admin@admins-Mac-mini.local>
2014-08-14 13:02:58 +0200

    Initial Commit

---
+++ VsMobile_FullWeb_SettingsView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VsMobile_FullWeb_SettingsView
+//
+//  Created by admin on 8/14/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VsMobile_FullWeb_SettingsViewTests/VsMobile_FullWeb_SettingsViewTests.m
@@ -0,0 +1,34 @@
+//
+//  VsMobile_FullWeb_SettingsViewTests.m
+//  VsMobile_FullWeb_SettingsViewTests
+//
+//  Created by admin on 8/14/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VsMobile_FullWeb_SettingsViewTests : XCTestCase
+
+@end
+
+@implementation VsMobile_FullWeb_SettingsViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
